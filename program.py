6d2268196b74320e1ca53dd8f6941b3ee86a2efe
Raghu0461 <44958979+Raghu0461@users.noreply.github.com>
2022-10-19 11:40:27 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,15 @@
+a=10
+b=20
+sum = a+b
+print("the sum is ",sum)
+
+def Hello(name):
+    print("This is",name)
+
+Hello("Kishan")
+
+
+num1=int(input("enter number1:"))
+num2=int(input("enter number2:"))
+sum=num1 + num2
+print("The total is", sum)
